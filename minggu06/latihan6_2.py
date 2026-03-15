7d5d3d325d686134049e333128fe83056de28568
febrian <r.fkr147@gmail.com>
2026-03-15 22:18:23 +0700

    revisi

--- minggu06/latihan6_2.py
+++ minggu06/latihan6_2.py
@@ -1,17 +1,12 @@
 def ganjil(bawah, atas):
-    try:
-        if bawah <= atas:
-            # Urutan naik: dari bawah ke atas
-            start = bawah if bawah % 2 != 0 else bawah + 1
-            for i in range(start, atas + 1, 2):
-                print(i, end=" ")
-        else:
-            # Urutan turun: dari bawah ke atas (bawah > atas)
-            start = bawah if bawah % 2 != 0 else bawah - 1
-            for i in range(start, atas - 1, -2):
-                print(i, end=" ")
-    except Exception as e:
-        print(f"Terjadi kesalahan: {e}")
+    if bawah <= atas:
+        start = bawah if bawah % 2 != 0 else bawah + 1
+        for i in range(start, atas + 1, 2):
+            print(i, end=" ")
+    else:
+        start = bawah if bawah % 2 != 0 else bawah - 1
+        for i in range(start, atas - 1, -2):
+            print(i, end=" ")
     
 bawah = int(input("Masukkan batas bawah: "))
 atas = int(input("Masukkan batas atas: "))
